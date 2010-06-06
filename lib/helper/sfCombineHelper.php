dfeed2131264d5227479d373f1af79bd537286ff
Kevin Dew <kev@kevin-dews-macbook.local>
2010-06-06 21:58:38 +0100

    Additions Grouping and inline tags

--- lib/helper/sfCombineHelper.php
+++ lib/helper/sfCombineHelper.php
@@ -1,159 +1,404 @@
 <?php
+/**
+ * sfCombine Helper Functions
+ * 
+ * @package     sfCombine
+ * @subpackage  Helper
+ * @author      Alexandre Mogères
+ * @author      Kevin Dew <kev@dewsolutions.co.uk>
+ */
 
-function include_combined_javascripts()
+/**
+ * Add a javascript file for grouping
+ *
+ * @param   string $js            The javascript filename
+ * @param   string $group         (Optional) The name of the group to be added
+ *                                to. Default empty string.
+ * @param   bool   $doNotCombine  (Optional) Whether to blacklist this file from
+ *                                combining. Default false
+ * @param   string $position      (Optional) @see sfWebResponse::addJavascript
+ * @param   array  $options       (Optional) @see sfWebResponse::addJavascript
+ * @return  void
+ */
+function use_javascript_grouped(
+  $js,
+  $group = '',
+  $doNotCombine = false,
+  $position = '',
+  $options = array()
+)
 {
-  echo get_combined_javascripts();
+  $manager = sfCombineManager::getJsManager();
+
+  sfContext::getInstance()
+           ->getResponse()
+           ->addJavascript($js, $position, $options);
+
+  $manager->addToGroup($group, $js);
+
+  if ($doNotCombine) {
+    $manager->addSkip($js);
+  }
 }
 
 /**
- * Returns <script> tags for all javascripts configured in view.yml or added to the response object.
+ * Output the combined javascripts
  *
- * You can use this helper to decide the location of javascripts in pages.
- * By default, if you don't call this helper, symfony will automatically include javascripts before </head>.
- * Calling this helper disables this behavior.
- *
- * @return string <script> tags
+ * @see get_combined_javascripts
  */
-function get_combined_javascripts()
+function include_combined_javascripts(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
 {
-  if (!sfConfig::get('app_sfCombinePlugin_enabled', false)) return get_javascripts();
-  
+  echo get_combined_javascripts($groups, $groupType);
+}
+
+/**
+ * Get the combined Javascripts in script links. Can get all groups or a
+ * selection. Calling this method will stop symfony automatically inserting
+ * scripts
+ *
+ *
+ * @param   mixed $groupsUse        (Optional) A string or array of groups to
+ *                                  include or exclude. Null for this to be
+ *                                  ignored. Default null.
+ * @param   int   $groupsUseType    (Optional) The type of grouping either
+ *                                  sfCombineManager::GROUP_INCLUDE or
+ *                                  sfCombineManager::GROUP_EXCLUDE.
+ *                                  These dictate whether the group(s) in
+ *                                  the previous argument should be marked
+ *                                  as used or every group marked as used.
+ *                                  Default sfCombineManager::GROUP_INCLUDE
+ * @param   bool  $onlyUnusedGroups (Optional) Only use unused groups. Default
+ *                                  true.
+ * @param   bool  $markGroupsUsed   (Optional) Mark the groups that are used
+ *                                  as used. Default true.
+ * @return  string
+ */
+function get_combined_javascripts(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
+{
+  // because of groups its better to run this through when sfCombine is disabled
+  // but just set all files to do not combine
+  $sfCombineEnabled = sfConfig::get('app_sfCombinePlugin_enabled', false);
+
+  $manager = sfCombineManager::getJsManager();
+
   sfConfig::set('symfony.asset.javascripts_included', true);
 
-  $html = '';
-  $jsFiles = array();
-  $regularJsFiles = array();
   $response = sfContext::getInstance()->getResponse();
   $config = sfConfig::get('app_sfCombinePlugin_js', array());
-  $doNotCombine = isset($config['combine_skip']) ? $config['combine_skip'] : array();
+  $doNotCombine = isset($config['combine_skip'])
+    ? $config['combine_skip']
+    : array();
+  $manager->setSkips(array_merge(
+    $manager->getSkips(),
+    $doNotCombine
+  ));
 
-  foreach ($response->getJavascripts() as $files => $options)
-  {
-    if (!is_array($files))
-    {
-      $files = array($files);
-    }
-    // check for js files that should not be combined
-    foreach ($files as $key => $value)
-    {
-      if (skip_asset($value, $doNotCombine))
-      {
-        array_push($regularJsFiles, $value);
-        unset($files[$key]);
-      }
-    }
-    $jsFiles = array_merge($jsFiles, $files);
-  }
+  $groupedFiles = $manager->getAssetsByGroup(
+    $response->getJavascripts(),
+    // handling disabled plugin
+    ($sfCombineEnabled ? $config['combine'] : false),
+    $groups,
+    $groupType,
+    $onlyUnusedGroups,
+    $markGroupsUsed,
+    array(sfCombineCombinerJs, 'getAssetPath')
+  );
+
+  $html = '';
+
+  $timestampConfig = sfConfig::get('app_sfCombinePlugin_timestamp', array());
+  foreach ($groupedFiles as $fileDetails) {
   
-  if (!empty($jsFiles))
-  {
-    $html .= str_replace(array('.js', '.pjs'), '', javascript_include_tag(url_for('sfCombine/js?key=' . _get_key($jsFiles))));
-  }
-  foreach ($regularJsFiles as $file)
-  {
-    $file = javascript_path($file);
-    $html .= javascript_include_tag($file);
+    if (!$fileDetails['combinable']) {
+
+      $file = $fileDetails['files'];
+
+      if (isset($timestampConfig['uncombinable'])
+        && $timestampConfig['uncombinable']
+        && $fileDetails['timestamp']
+      ) {
+        if (strpos($file, '?') !== false) {
+          $file .= '&t=' . $fileDetails['timestamp'];
+        } else {
+          $file .= '?t=' . $fileDetails['timestamp'];
+        }
+      }
+
+      $html .= javascript_include_tag(
+        javascript_path($file),
+        $fileDetails['options']
+      );
+    } else {
+
+      $route = isset($config['route']) ? $config['route'] : 'sfCombine';
+
+      $html .= javascript_include_tag(
+        url_for(
+          '@' . $route . '?module=sfCombine&action=js&'
+          . sfCombineUrl::getUrlString($fileDetails['files'], $fileDetails['timestamp'])
+        ),
+        $fileDetails['options']
+      );
+    }
   }
   
   return $html;
 }
 
-function include_combined_stylesheets()
+/**
+ * @see use_javascript_grouped
+ */
+function use_stylesheet_grouped(
+  $css,
+  $group = '',
+  $doNotCombine = false,
+  $position = '',
+  $options = array()
+)
+{
+  $manager = sfCombineManager::getCssManager();
+
+  sfContext::getInstance()
+           ->getResponse()
+           ->addStylesheet($css, $position, $options);
+
+  $manager->addToGroup($group, $css);
+
+  if ($doNotCombine) {
+    $manager->addSkip($css);
+  }
+}
+
+/**
+ * @see include_combined_javascripts
+ */
+function include_combined_stylesheets(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
 {
   echo get_combined_stylesheets();
 }
 
 /**
- * Returns <link> tags with the url toward all stylesheets configured in view.yml or added to the response object.
- *
- * You can use this helper to decide the location of stylesheets in pages.
- * By default, if you don't call this helper, symfony will automatically include stylesheets before </head>.
- * Calling this helper disables this behavior.
- *
- * @return string <link> tags
+ * @see get_combined_javascripts
  */
-function get_combined_stylesheets()
+function get_combined_stylesheets(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
 {
-  if (!sfConfig::get('app_sfCombinePlugin_enabled', false)) return get_stylesheets();
-  
+  // because of groups its better to run this through when sfCombine is disabled
+  // but just set all files to do not combine
+  $sfCombineEnabled = sfConfig::get('app_sfCombinePlugin_enabled', false);
+
+  $manager = sfCombineManager::getCssManager();
+
   sfConfig::set('symfony.asset.stylesheets_included', true);
-  
-  $html = '';
-  $cssFiles = array();
+
   $response = sfContext::getInstance()->getResponse();
-  foreach ($response->getStylesheets() as $files => $options)
-  {
-    if (!is_array($files))
-    {
-      $files = array($files);
+  $config = sfConfig::get('app_sfCombinePlugin_css', array());
+  $doNotCombine = isset($config['combine_skip'])
+    ? $config['combine_skip']
+    : array();
+  $manager->setSkips(array_merge(
+    $manager->getSkips(),
+    $doNotCombine
+  ));
+
+  $groupedFiles = $manager->getAssetsByGroup(
+    $response->getStylesheets(),
+    // handling disabled plugin
+    ($sfCombineEnabled ? $config['combine'] : false),
+    $groups,
+    $groupType,
+    $onlyUnusedGroups,
+    $markGroupsUsed,
+    array(sfCombineCombinerCss, 'getAssetPath')
+  );
+
+  $html = '';
+
+  foreach ($groupedFiles as $fileDetails) {
+    if (!$fileDetails['combinable']) {
+
+      // @todo handle timestamp
+
+      $html .= stylesheet_tag(
+        stylesheet_path($fileDetails['files']),
+        $fileDetails['options']
+      );
+    } else {
+
+      $route = isset($config['route']) ? $config['route'] : 'sfCombine';
+
+      $html .= stylesheet_tag(
+        url_for(
+          '@' . $route . '?module=sfCombine&action=css&'
+          . sfCombineUrl::getUrlString(
+              $fileDetails['files'], $fileDetails['timestamp']
+            )
+        ),
+        $fileDetails['options']
+      );
     }
-    $cssFiles = array_merge($cssFiles, $files);
   }
-  
-  if (!empty($cssFiles))
-  {
-    $html .= str_replace(array('.css', '.pcss'), '', stylesheet_tag(url_for('sfCombine/css?key=' . _get_key($cssFiles))));
-  }
-  
+
   return $html;
 }
 
 /**
- * Returns a key which combined all assets file
+ * Method to get both stylesheets and javascripts through one function call
  *
- * @return string md5
+ * @see include_combined_javacripts
  */
-function _get_key($files)
+function include_combined_assets(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
 {
-  $content = base64_encode(serialize($files));
-  $key = md5($content . sfConfig::get('app_sfCombinePlugin_asset_version', ''));
-
-  if (!class_exists('DbFinder'))
-  {
-    throw new Exception('sfCombine expects DbFinder to call combined asset file');
-  }
-
-  if (function_exists('apc_store') && ini_get('apc.enabled')) 
-  {
-    $cache = new sfAPCCache();
-    // FIXME: APCCache "has" method doesn't work 
-    $checkFunction = apc_fetch($key);
-  }
-  else
-  {
-    $cache = new sfFileCache(array('cache_dir'=>sfConfig::get('sf_cache_dir').'/combiners'));
-    $checkFunction = $cache->has($key);
-  }
-
-  // Checks if key exists
-  if (false === $checkFunction)
-  {
-    $keyExists = DbFinder::from('sfCombine')->
-      where('AssetsKey', $key)->
-      count();  
-    if (!$keyExists)
-    {
-      $combine = new sfCombine();
-      $combine->setAssetsKey($key);
-      $combine->setFiles($content);
-      $combine->save();
-    }
-    $cache->set($key, true);
-  }
-
-  return $key;
+  echo get_combined_assets(
+    $groups, $groupsType, $onlyUnusedGroups, $markGroupsUsed
+  );
 }
 
 /**
- * Checks if an asset has to be skipped by the merge
+ * Method to get both stylesheets and javascripts through one function call
  *
- * @param string $value
- * @param array  $doNotCombine
- *
- * @return boolean
+ * @see get_combined_javacripts
  */
-function skip_asset($value, $doNotCombine)
+function get_combined_assets(
+  $groups = null,
+  $groupType = sfCombineManager::GROUP_INCLUDE,
+  $onlyUnusedGroups = true,
+  $markGroupsUsed = true
+)
 {
-  $extensions = array('.js', '.css', '.pjs', 'pcss');
-  return in_array(str_replace($extensions, '', $value), str_replace($extensions, '', $doNotCombine));
+  return get_combined_javascripts(
+    $groups, $groupType, $onlyUnusedGroups, $markGroupsUsed
+  ) . get_combined_stylesheets(
+    $groups, $groupType, $onlyUnusedGroups, $markGroupsUsed
+  );
+}
+
+/**
+ * @see     javascript_tag
+ * @param   string $content
+ * @return  string
+ */
+function javascript_tag_minified($content = null)
+{
+  use_helper('JavascriptBase');
+  
+  if (null === $content) {
+    ob_start();
+
+  } else {
+    // minify content
+    $config = sfConfig::get('app_sfCombinePlugin_js', array());
+    $combinerClass = isset($config['combiner_class'])
+                   ? $config['combiner_class']
+                   : 'sfCombineCombinerJs';
+
+    $combiner = new $combinerClass(
+      $config
+    );
+
+    $content = $combiner->minify(
+      $content,
+      (isset($config['inline_minify_method']) 
+        ? $config['inline_minify_method'] 
+        : false
+      ),
+      (isset($config['inline_minify_method_options']) 
+        ? $config['inline_minify_method_options'] 
+        : array()
+      )
+    );
+
+    return javascript_tag($content);
+  }
+}
+
+/**
+ * @see     end_javascript_tag
+ * @return  void
+ */
+function end_javascript_tag_minified()
+{
+  $js = ob_get_clean();
+  echo javascript_tag_minified($js);
+}
+
+/**
+ * @see     javascript_tag_minified
+ * @param   string $content
+ * @return  string
+ */
+function style_tag_minified($content = null, array $elementOptions = array())
+{
+  use_helper('Tag');
+
+  if (null === $content) {
+    ob_start();
+
+  } else {
+    // minify content
+    $config = sfConfig::get('app_sfCombinePlugin_css', array());
+    $combinerClass = isset($config['combiner_class'])
+                   ? $config['combiner_class']
+                   : 'sfCombineCombinerCss';
+
+    $combiner = new $combinerClass(
+      $config
+    );
+
+    $content = $combiner->minify(
+      $content,
+      (isset($config['inline_minify_method'])
+        ? $config['inline_minify_method']
+        : false
+      ),
+      (isset($config['inline_minify_method_options'])
+        ? $config['inline_minify_method_options']
+        : array()
+      )
+    );
+
+    if (!isset($elementOptionsp['type'])) {
+      $elementOptions['type'] = 'text/css';
+    }
+
+    return content_tag(
+      'style',
+      "\n/*" . cdata_section("*/\n$content\n/*") . "*/\n",
+      $elementOptions
+    );
+  }
+}
+
+/**
+ * @see     end_javascript_tag
+ * @return  void
+ */
+function end_style_tag_minified(array $elementOptions = array())
+{
+  $css = ob_get_clean();
+  echo style_tag_minified($css);
 }
