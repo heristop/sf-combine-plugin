563be68f77dcd0ea9e0a0d1c5384afd960ac9955
Kevin Dew <kev@kevin-dews-macbook.local>
2010-06-12 13:12:08 +0100

    Renamed options for greater yaml consistency

--- lib/minifier/sfCombineMinifierPacker.class.php
+++ lib/minifier/sfCombineMinifierPacker.class.php
@@ -14,7 +14,7 @@ class sfCombineMinifierPacker implements sfCombineMinifierInterface
    * @param   $options    (Optional) array params
    *                      - encoding: 'None', 'Numeric', 'Normal', 'High ASCII'
    *                      (string/int - default 'Normal')
-   *                      - fastDecode: include the fast decoder in the packed
+   *                      - fast_decode: include the fast decoder in the packed
    *                      result (bool - default true
    *                      - specialChars: if you are flagged your private and
    *                      local variables (bool - default false
@@ -24,8 +24,8 @@ class sfCombineMinifierPacker implements sfCombineMinifierInterface
     $packer = new JavaScriptPacker(
       $content,
       (isset($options['encoding']) ? $options['encoding'] : 'Normal'),
-      (isset($options['fastDecode']) ? $options['fastDecode'] : true),
-      (isset($options['specialChars']) ? $options['specialChars'] : false)
+      (isset($options['fast_decode']) ? $options['fast_decode'] : true),
+      (isset($options['special_chars']) ? $options['special_chars'] : false)
     );
 
     return $packer->pack();
